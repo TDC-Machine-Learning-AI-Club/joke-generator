dda764122c0a04a45c8ae2911ba1aa8b6267960e
Gervas Mshamu <gervas.mshamu@eganet.go.tz>
2024-01-04 17:38:38 +0300

    Add: comments for readability

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import gradio as gr
 import requests
 
-API_KEY = ""
+"""
+    Get your API key from https://makersuite.google.com/app/apikey 
+"""
+
+API_KEY = "" #Enter your API key
 API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={API_KEY}".format(API_KEY=API_KEY)
 
 
@@ -22,6 +26,11 @@ def processor(input):
     response = requests.post(API_URL,json=payload)
 
     return response.json()['candidates'][0]['content']['parts'][0]['text']
+
+"""
+    We have used the gradio framework,
+    Full documentation visit https://www.gradio.app/
+"""
 
 inputs = gr.Textbox(lines=2, placeholder="Eg: Tell me a coffee shop joke",label="Input Text")
 outputs = gr.Textbox(label="Joke(s) Generated")
